7cc04942850569ba1abd249c5d6e123bbf8221e6
Gabriel Matos <matosruelagabriel@gmail.com>
2026-03-13 15:45:56 -0300

    v0.2
    feat: add roleta command with embed and images, add 8ball and update commands, improve help command

---
+++ changelog.md
@@ -0,0 +1,18 @@
+## V.02 - 13/03/2026
+
+## Added
+- Comando `$8ball`
+- Comando `$update`
+- Comando `$roleta` (Roleta Russa)
+
+### Changed
+- Estilizado o comando `$roleta` com embed
+
+### Fixed
+- Comando `$avatar` não exige mais menção (@)
+
+### Optimization
+- Bot agora aceita comandos em maiúsculas ou minúsculas
+
+## Coming..
+- Estilização de outros comandos
--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import discord
 import random
-import os
 from discord.ext import commands
+from dotenv import load_dotenv
+from datetime import timedelta
+import os
 from deep_translator import GoogleTranslator
 
-
+load_dotenv()
 token = os.getenv("TOKEN")
+print(token)
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="$",
+                    intents=intents,
+                    case_insensitive=True)
 
 @bot.event
 async def on_ready():
@@ -21,27 +26,44 @@ async def ping(ctx):
     await ctx.send("Pong!")
 
 @bot.command(aliases=["foto","f"])
-async def avatar(ctx,member:discord.Member):
+async def avatar(ctx,member:discord.Member = None):
+    if member is None:
+        member = ctx.author
     await ctx.send(member.avatar.url)
 
-@bot.command(aliases=["es","Es","ES","eS"])
+@bot.command(aliases=["es"])
 async def espanhol(ctx, *, texto):
     traducao = GoogleTranslator(source='auto', target="es").translate(texto)
     await ctx.send(traducao)
 
-@bot.command(aliases=["pt","Pt","pT",'PT'])
+@bot.command(aliases=["pt"])
 async def portugues(ctx, *, texto):
     traducao = GoogleTranslator(source='auto', target="pt").translate(texto)
     await ctx.send(traducao)
 
-@bot.command(aliases=["en","EN","En","eN"])
+@bot.command(aliases=["en"])
 async def ingles(ctx, *, texto):
     traducao = GoogleTranslator(source='auto', target="en").translate(texto)
     await ctx.send(traducao)
 
 @bot.command(aliases=["ayuda"])
 async def ajuda(ctx):
-    await ctx.send("Meu comandos são : $PING , $EN , $ES , $PT , $AVATAR @ ",)
+    await ctx.send("📖 **Comandos do Bot**\n\n"
+        "🏓 **$ping** → Verifica se o bot está online\n\n"
+        
+        "🌎 **Tradução:**\n"
+        "$en <texto> → Traduz para inglês\n"
+        "$es <texto> → Traduz para espanhol\n"
+        "$pt <texto> → Traduz para português\n\n"
+        
+        "👤 **$avatar [@usuario]** → Mostra o avatar de um usuário (ou o seu se não marcar ninguém)\n\n"
+        
+        "🎲 **$roleta** → Jogue roleta russa (1/6 de chance de ser silenciado)\n\n"
+        
+        "🎱 **$8ball <pergunta>** → Faça uma pergunta e receba uma resposta aleatória\n\n"
+        
+        "📰 **$update** → Mostra as últimas atualizações do bot"
+    )
 
 @bot.command()
 async def cor(ctx, cor: str):
@@ -94,6 +116,50 @@ async def quem(ctx, *, pergunta):
 
     await ctx.send(f"{pergunta}\n é : {escolhido.mention}")
 
+@bot.command(aliases=["8ball"])
+async def ball(ctx):
+    lista = ['sim','não','talvez']
+    await ctx.send(random.choice(lista))
+
+@bot.command()
+async def update(ctx):
+    with open("changelog.md") as change:     
+        await ctx.send(change.read())
+
+@bot.command()
+async def roleta(ctx):
+    resultado = random.randint(1,6)
+    print(resultado)
+    morte = 1
+    if resultado == morte:
+        embed = discord.Embed(
+        title="🎲 Roleta Russa",
+        description="💥 BANG! Você morreu e foi silenciado por 30s",
+        color= discord.Color.red()
+    )
+        arquivo = discord.File("images/morreu.png", filename="morreu.png")
+        embed.set_image(url="attachment://morreu.png")
+        embed.set_footer(text=f"Executado por {ctx.author.name}")
+        embed.set_thumbnail(url=ctx.author.avatar.url)
+        try:
+            await ctx.author.timeout(timedelta(seconds=30))
+        except discord.errors.Forbidden:
+            print("Erro de permissão")
+        await ctx.send(embed=embed,file=arquivo)
+
+    else:
+            embed = discord.Embed(
+            title="🎲 Roleta Russa",
+            description="*click*...Você sobreviveu por enquanto",
+            color= discord.Color.green()
+    )
+            arquivo =discord.File("images/viveu.png", filename="viveu.png")
+            embed.set_image(url="attachment://viveu.png")
+            embed.set_footer(text=f"Executado por {ctx.author.name}")
+            embed.set_thumbnail(url=ctx.author.avatar.url)
+            await ctx.send(embed=embed,file=arquivo)
+
+    
 
 bot.run(token)
 
